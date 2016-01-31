f5927ca2595e9548b11e5cd9dcf7558ad9b91e45
Ludmil Bojilov <ludmil.s.b@gmail.com>
2016-01-31 11:43:36 +0200

    First Commit

---
+++ TestsRunner/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TestsRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Create XML";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Select Cases";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(12, 95);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(296, 219);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(251, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 20);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(320, 326);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ TestsRunner/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Xml;
+
+namespace TestsRunner
+{
+    public partial class Form1 : Form
+    {
+        private string path = "";
+        public StackPanel stackPanel;
+        System.Windows.Controls.TreeView treeV;
+        public ScrollViewer scrollView;
+        public List<string> selectedCases = new List<string>();
+        Window a;
+        XmlNodeList xnList = null;
+        OpenFileDialog dlg = new OpenFileDialog();
+        List<XmlNode> nodes = new List<XmlNode>();
+        Window win = null;
+        Dictionary <string[],string> list = new Dictionary<string[],string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            dlg.DefaultExt = ".xml";
+            Nullable<DialogResult> result = dlg.ShowDialog();
+            if (!result.HasValue.Equals(null))
+            {
+                path = dlg.FileName;
+                label1.Text = path;
+                if (dlg.CheckPathExists)
+                {
+                    filename = Path.GetFileName(path);
+                }
+                if (!String.IsNullOrEmpty(filename))
+                {
+                    textBox2.AppendText("Document '" + filename + "' Loaded \n");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xml = new XmlDocument();
+            if (!String.IsNullOrEmpty(path))
+            {
+                xml.Load(path);
+                string criteria = "/Tests/readyToRun/Test";
+                xnList = xml.SelectNodes(criteria);
+                foreach (XmlNode xmln in xnList)
+                {
+                    string[] toAdd = new string[2];
+                    if (xmln.OuterXml.Contains("critical=\"Y\""))
+                    {
+                        toAdd[0] = xmln["Name"].InnerText;
+                        toAdd[1] = "critical";
+                    }
+                    else
+                    {
+                        toAdd[0] = xmln["Name"].InnerText;
+                        toAdd[1] = "normal";
+                    }
+                    list.Add(toAdd,xmln["Category"].InnerText);
+                }
+                win = Window2(path, list);
+                win.ShowDialog();
+            }
+        }
+        
+        public Window Window2(string path, Dictionary<string[], string> list)
+        {
+            a = new Window();
+            stackPanel = new StackPanel { Orientation = System.Windows.Controls.Orientation.Vertical };
+            scrollView = new ScrollViewer { HorizontalScrollBarVisibility = ScrollBarVisibility.Auto };
+            a.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            a.WindowStyle = WindowStyle.None;
+            a.Height = 700;
+            a.Width = 500;
+            a.Title = path;
+            a.ResizeMode = ResizeMode.NoResize;
+            treeV = new System.Windows.Controls.TreeView { Name = "Tests" };
+
+            List<TreeViewItem> ltv = new List<TreeViewItem>();
+            List<string> containsE = new List<string>();
+
+            foreach (KeyValuePair<string[],string> item in list)
+            {
+                if (!containsE.Contains(item.Value))
+                {
+                    containsE.Add(item.Value);
+                    TreeViewItem itm = new TreeViewItem();
+                    itm.Header = item.Value;
+                    ltv.Add(itm);
+                }
+            }
+
+            foreach (TreeViewItem im in ltv)
+            {
+                treeV.Items.Add(im);
+            }
+
+            foreach (KeyValuePair<string[], string> item in list)
+            {
+                insertItems(item, treeV);
+            }
+
+            var selectBtn = new System.Windows.Controls.Button { Content = "Select" };
+            System.Windows.Controls.Button closeBtn = new System.Windows.Controls.Button { Name = "closeBtn", Content = "Close" };
+            stackPanel.Children.Add(treeV);
+            stackPanel.Children.Add(selectBtn);
+            stackPanel.Children.Add(closeBtn);
+            scrollView.Content = stackPanel;
+            a.Content = scrollView;
+            closeBtn.Click += new RoutedEventHandler(onClosing);
+            selectBtn.Click += new RoutedEventHandler(selectCases);
+            return a;
+        }
+
+        public void insertItems(KeyValuePair<string[], string> item, System.Windows.Controls.TreeView trev)
+        {
+            if (trev.HasItems)
+            {
+                foreach (TreeViewItem trvit in trev.Items)
+                {
+                    if (trvit.Header.Equals(item.Value))
+                    {
+                        if (item.Key[1].Equals("critical"))
+                        {
+                            trvit.Items.Add(new System.Windows.Controls.CheckBox
+                            {
+                                FontStyle = FontStyles.Italic,
+                                Foreground = new SolidColorBrush(Colors.Orange),
+                                Content = item.Key[0]
+                            });
+                        }
+                        else
+                        {
+                            trvit.Items.Add(new System.Windows.Controls.CheckBox { Content = item.Key[0] });
+                        }
+                    }
+                }
+            }
+        }
+
+        public TreeViewItem addToTreeView(string itemValue,string[] itemKey)
+        {
+            TreeViewItem treeviewitem = new TreeViewItem { Header = itemValue };
+            if (itemKey[1].Equals("critical"))
+            {
+                treeviewitem.Items.Add(new System.Windows.Controls.CheckBox
+                {
+                    FontStyle = FontStyles.Italic,
+                    Foreground = new SolidColorBrush(Colors.Orange),
+                    Content = itemKey[0]
+                });
+            }
+            else
+            {
+                treeviewitem.Items.Add(new System.Windows.Controls.CheckBox { Content = itemKey[0] });
+            }
+            return treeviewitem;
+        }
+
+        private void selectCases(object sender, RoutedEventArgs e)
+        {
+            stackPanel.Children.RemoveAt(stackPanel.Children.Count - 1);
+            stackPanel.Children.RemoveAt(stackPanel.Children.Count - 1);
+
+            foreach (TreeViewItem item in treeV.Items)
+            {
+                foreach (System.Windows.Controls.CheckBox chb in item.Items)
+                {
+                    if (chb.IsChecked.Equals(true))
+                    {
+                        selectedCases.Add(chb.Content.ToString());
+                    }
+                }
+                
+            }
+            textBox2.AppendText(selectedCases.Count + " Cases Added for Running \n");
+            button4_Click(sender, e);
+        }
+
+        private void onClosing(object sender, RoutedEventArgs e)
+        {
+            win.Close();
+            a.Close();
+            a.Content = null;
+            stackPanel.Children.Clear();
+            path = "";
+            list.Clear();
+            label1.Text = "";
+            treeV.Items.Clear();
+            nodes.Clear();
+            selectedCases.Clear();
+            textBox2.AppendText("Cases Window closed! \n");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(path) && !String.IsNullOrEmpty(label1.Text))
+            {
+                win.Close();
+                a.Close();
+                a.Content = null;
+                stackPanel.Children.Clear();
+                path = "";
+                list.Clear();
+                label1.Text = "";
+                treeV.Items.Clear();
+                nodes.Clear();
+                selectedCases.Clear();
+                textBox2.AppendText("Path cleared \n");
+            }
+            else
+            {
+                textBox2.AppendText("Path already cleared \n");
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (XmlNode xmln in xnList)
+            {
+                foreach (string item in selectedCases)
+                {
+                    if (xmln["Name"].InnerText.Equals(item))
+                    {
+                        nodes.Add(xmln);
+                    }
+                }               
+            }
+
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode testsNode = xmlDoc.CreateElement("Tests");
+            xmlDoc.AppendChild(testsNode);
+            XmlNode readyToRunNode = xmlDoc.CreateElement("readyToRun");
+            testsNode.AppendChild(readyToRunNode);
+            foreach (XmlNode xmln in nodes)
+            {
+                try
+                {
+                    XmlNode import = xmlDoc.ImportNode(xmln,true);
+                    readyToRunNode.AppendChild(import);
+                }
+                catch (Exception ex)
+                {
+                    textBox2.AppendText("Exception orccured while importing the xml nodes : " + ex.Message + "\n" );
+                }
+                
+            }
+            try
+            {
+                if (File.Exists("Tests.xml"))
+                {
+                    textBox2.AppendText("Deleting old document \n");
+                    File.Delete("Tests.xml");
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox2.AppendText("Exception orccured  while saving the xml file : " + ex.Message + "\n");
+            }
+            xmlDoc.Save("Tests.xml");
+            textBox2.AppendText("New document created \n");
+            
+            a.Content = null;
+            stackPanel.Children.Clear();
+            selectedCases.Clear();
+            path = "";
+            list.Clear();
+            label1.Text = "";
+            treeV.Items.Clear();
+            nodes.Clear();
+        }
+    }
+}
